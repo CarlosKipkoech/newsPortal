a7edc005c5462df90b3f7ed2523701deb794babb
carlos kipkoech <carlos.kipkoech@student.moringaschool.com>
2021-11-16 20:42:39 +0300

    add tests

--- src/test/java/models/DepartmentNewsTest.java
+++ src/test/java/models/DepartmentNewsTest.java
@@ -1,4 +1,49 @@
 package models;
 
-public class DepartmentNewsTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DepartmentNewsTest{
+    private User setUpNewUser() {
+        return new User("John","manager",5);
+    }
+    @Test
+    public void newuserInstantiatesCorrectly_true() throws Exception {
+        User testUser = setUpNewUser();
+        assertTrue(true);
+    }
+
+
+
+    @Test
+    public void newuser_getsName_String() throws Exception {
+        User testUser = setUpNewUser();
+        assertEquals("john",testUser.getName());
+    }
+
+    @Test
+    public void getAge__getsDescription_manager() throws Exception {
+        User testUser = setUpNewUser();
+        assertEquals("manager", testUser.getRole());
+    }
+
+    @Test
+    public void getPower_getsNumberofemployees_String() throws Exception{
+        User testUser = setUpNewUser();
+        assertEquals(5, testUser.getDepartmentId());
+    }
+
+
+
+
+
+
+
+    @Test
+    public void getId_userInstantiatesWithAnId_0() {
+        User testUser = setUpNewUser();
+        assertEquals(0,testUser.getId());
+    }
 }
